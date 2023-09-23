f4b2abff9363bfa85447881b4de4a57bdda13f64
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-09-23 12:01:35 -0400

    feat: types from api with zod

--- src/lib/exchange-schemas.ts
+++ src/lib/exchange-schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const InputSchema = z.object({
-  amount: z
+  amount: z.coerce
     .number()
     .positive()
     .refine(
@@ -11,20 +11,41 @@ export const InputSchema = z.object({
         message: "Invalid amount provided",
       }
     ),
+  from: z.string().length(3),
+  to: z.string().length(3),
+});
+const currencies = z.record(z.string().length(3), z.string());
+const error = z.object({
+  code: z.number(),
+  info: z.string(),
 });
 
-// Schema for an individual symbol
-const symbolSchema = z.object({
-  code: z.string().length(3),
-  name: z.string(),
+export const getCurrenciesResponse = z.discriminatedUnion("success", [
+  z.object({ success: z.literal(true), currencies }),
+  z.object({ success: z.literal(false), error }),
+]);
+
+export const convertCurrenciesResponse = z.discriminatedUnion("success", [
+  z.object({
+    success: z.literal(true),
+    result: z.number(),
+    info: z.object({ quote: z.number(), timestamp: z.number() }),
+    query: z.object({ amount: z.number(), from: z.string(), to: z.string() }),
+  }),
+  z.object({ success: z.literal(false), error }),
+]);
+
+const currencySchema = z.object({
+  code: currencies.keySchema,
+  name: currencies.valueSchema,
 });
 
-// Schema for the overall response
-const symbolsResponseSchema = z.object({
-  success: z.boolean(),
-  symbols: z.record(symbolSchema),
-});
-
-export type Amount = z.infer<typeof InputSchema>;
-export type Symbol = z.infer<typeof symbolSchema>;
-export type SymbolResponse = z.infer<typeof symbolsResponseSchema>;
+export type InputRequest = z.infer<typeof InputSchema>;
+export type Currency = z.infer<typeof currencySchema>;
+export type SuccessConvertResponse = z.infer<
+  typeof convertCurrenciesResponse
+> extends infer U
+  ? U extends { success: true }
+    ? Omit<U, "success">
+    : never
+  : never;
